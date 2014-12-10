85f146e712d87163c1c91009ad44fb848d35eb43
Mohd Irtefa <irtefa1@illinois.edu>
2014-12-09 17:53:47 -0800

    configured it to server json

--- jobr.go
+++ jobr.go
@@ -2,14 +2,12 @@ package main
 
 import (
     "net/http"
-    "fmt"
     "os"
-    // "io"
-    // "encoding/json"
+    "io"
+    "encoding/json"
 )
 
 func main() {
-    fmt.Printf("Hello")
     http.HandleFunc("/", handler)
 
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
@@ -23,8 +21,7 @@ type Result struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "application/json")
-    // result, _ := json.Marshal(Result{"hello Jobr"})
-    // io.WriteString(w, string(result))
-    fmt.Fprint(w, "Hello, world!")
+    w.Header().Set("Content-Type", "application/json")
+    result, _ := json.Marshal(Result{"hello Jobr"})
+    io.WriteString(w, string(result))
 }
